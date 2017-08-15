ba11099dffb86b60b5eab87851e2b2827418e8c5
Josiah Savary <hello@jsavary.com>
2017-08-15 01:26:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
 
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+const App = () => (
+  <div style={styles}>
+    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
